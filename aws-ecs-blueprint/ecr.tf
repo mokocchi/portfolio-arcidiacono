90fe855865d6a4071f1333a597fbabdbbb800ae0
Jose Arcidiacono <j2arcidiacono@outlook.com>
2025-12-07 20:53:15 -0300

    Create ecr.tf

---
+++ aws-ecs-blueprint/ecr.tf
@@ -0,0 +1,26 @@
+resource "aws_ecr_repository" "app" {
+  for_each = var.app_images
+
+  name = each.value.name
+
+  image_tag_mutability = "MUTABLE"
+
+  lifecycle_policy {
+    policy = jsonencode({
+      rules = [
+        {
+          rulePriority = 1
+          description  = "Keep last 10 images"
+          selection = {
+            tagStatus     = "any"
+            countType     = "imageCountMoreThan"
+            countNumber   = 10
+          }
+          action = {
+            type = "expire"
+          }
+        }
+      ]
+    })
+  }
+}
